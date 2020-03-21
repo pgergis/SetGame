5ea565fab08e69e97503c70ade1aa699ad79b953
Pierre Gergis <pierre.gergis@candidco.com>
2020-03-21 19:52:08 -0400

    Initial Commit

---
+++ SetGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SetGame
+//
+//  Created by Pierre Gergis on 3/21/20.
+//  Copyright © 2020 Pierre Gergis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
